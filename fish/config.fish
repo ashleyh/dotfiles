0817081f652a52d99d831fe6f6292510913b50c5
aph <a.hewson@gmail.com>
2014-01-19 21:58:06 +0000

    fish aliases

---
+++ fish/config.fish
@@ -0,0 +1,29 @@
+alias g=grep
+alias ga='git add'
+alias gc='git commit -v'
+alias gca='git commit -v -a'
+alias gcl='git clone'
+alias gd='git diff'
+alias gdc='git diff --cached'
+alias gdvc='git diff --cached'
+alias gl='git pull --ff-only'
+alias glg='git log --stat --max-count=10'
+alias glgg='git log --graph --max-count=10'
+alias glgga='git log --graph --decorate --all'
+alias glggg='git log --graph --decorate --all --max-count=100'
+alias gp='git push'
+alias gst='git status'
+alias h=history
+alias httpd='py -m SimpleHTTPServer'
+alias ipy=ipython
+alias l=ls
+alias ll='l -lh'
+alias py=python2
+alias py3=python3
+alias q=exit
+alias v=vim
+alias view='vim -R'
+alias vv='v ~/dotfiles/.vimrc'
+alias vz='v ~/dotfiles/zsh'
+alias ys='yaourt -S'
+alias yss='yaourt -Ss'
