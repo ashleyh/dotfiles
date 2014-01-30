2a18949f070bb9db76520b1f9930354d858087c9
aph <a.hewson@gmail.com>
2014-01-30 20:18:15 +0000

    zsh: put $TTY in title

--- zsh/111_title.zsh
+++ zsh/111_title.zsh
@@ -1,1 +1,33 @@
-source $ZSH/lib/termsupport.zsh
+#from omz/lib/termsupport.zsh
+
+#usage: title short_tab_title looooooooooooooooooooooggggggg_windows_title
+#http://www.faqs.org/docs/Linux-mini/Xterm-Title.html#ss3.1
+#Fully support screen, iterm, and probably most modern xterm and rxvt
+#Limited support for Apple Terminal (Terminal can't set window or tab separately)
+function title {
+  if [[ "$DISABLE_AUTO_TITLE" == "true" ]] || [[ "$EMACS" == *term* ]]; then
+    return
+  fi
+  if [[ "$TERM" == screen* ]]; then
+    print -Pn "\ek$1:q\e\\" #set screen hardstatus, usually truncated at 20 chars
+  elif [[ "$TERM" == xterm* ]] || [[ $TERM == rxvt* ]] || [[ $TERM == ansi ]] || [[ "$TERM_PROGRAM" == "iTerm.app" ]]; then
+    print -Pn "\e]2;$2:q\a" #set window name
+    print -Pn "\e]1;$1:q\a" #set icon (=tab) name (will override window name on broken terminal)
+  fi
+}
+
+#Appears when you have the prompt
+function omz_termsupport_precmd {
+  local title="$TTY"
+  title "$title" "$title"
+}
+
+#Appears at the beginning of (and during) of command execution
+function omz_termsupport_preexec {
+  local title="$TTY"
+  title "$title" "$title"
+}
+
+autoload -U add-zsh-hook
+add-zsh-hook precmd  omz_termsupport_precmd
+add-zsh-hook preexec omz_termsupport_preexec
