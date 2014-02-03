de2df2eab9b4cd117e71fd5c7fe9e9ecd85e0772
aph <a.hewson@gmail.com>
2014-02-03 10:02:27 +0000

    brew aliases

--- zsh_bits/600_darwin.zsh
+++ zsh_bits/600_darwin.zsh
@@ -11,4 +11,12 @@ HELPDIR=/usr/local/share/zsh/helpfiles
 
 export PIP_DOWNLOAD_CACHE=$HOME/Library/Caches/pip-downloads
 
-
+alias b='brew'
+alias bs='brew search'
+alias bi='brew install'
+alias binf='brew info'
+alias biv='brew install -v'
+alias br='brew rm'
+alias bu='brew update'
+alias be='brew edit'
+alias bl='brew ls'
