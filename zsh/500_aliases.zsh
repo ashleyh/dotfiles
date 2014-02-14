26d19c3ea7b87c0249f53f92f145295ea4e2b512
aph <a.hewson@gmail.com>
2014-02-14 09:15:02 +0000

    zsh: sort aliases

--- zsh/500_aliases.zsh
+++ zsh/500_aliases.zsh
@@ -1,22 +1,22 @@
 
-alias history='fc -l 1'
-alias v=vim
 alias apts='aptitude search'
-alias sai='sudo aptitude install'
-alias py='python2'
-alias py3='python3'
+alias dig='dig +short'
 alias g='grep'
-alias q='exit'
-alias t='python ~/apps/t/t.py --task-dir ~/misc/tasks --list tasks'
+alias h='history'
+alias history='fc -l 1'
+alias httpd='py -m SimpleHTTPServer'
 alias ipy='ipython' # as opposed to ironpython
-alias m='./manage.py'
 alias ll='l -lh'
+alias m='./manage.py'
+alias py3='python3'
+alias py='python2'
+alias q='exit'
+alias sai='sudo aptitude install'
+alias t='python ~/apps/t/t.py --task-dir ~/misc/tasks --list tasks'
+alias v=vim
+alias vi3='v ~/dotfiles/.i3/config'
 alias vv='v ~/dotfiles/.vimrc'
 alias vz='v ~/dotfiles/zsh'
-alias vi3='v ~/dotfiles/.i3/config'
-alias httpd='py -m SimpleHTTPServer'
-alias dig='dig +short'
-alias h='history'
 
 function hgdv() {
   hg diff "$@" | view -
