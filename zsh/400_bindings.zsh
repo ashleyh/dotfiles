214c7b6adcdf69866ea46d84ea91d470fa045066
aph <a.hewson@gmail.com>
2014-04-10 22:46:28 +0100

    zsh: tweak arrow key bindings

--- zsh/400_bindings.zsh
+++ zsh/400_bindings.zsh
@@ -10,9 +10,8 @@ bindkey -e
 bindkey "^[[5~" up-line-or-history
 bindkey "^[[6~" down-line-or-history
 
-# make search up and down work, so partially type and hit up/down to find relevant stuff
-bindkey '^[[A' up-line-or-search
-bindkey '^[[B' down-line-or-search
+bindkey '^[[A' history-beginning-search-backward
+bindkey '^[[B' history-beginning-search-forward
 
 bindkey "^[[H" beginning-of-line
 bindkey "^[[1~" beginning-of-line
